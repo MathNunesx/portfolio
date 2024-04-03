27d5fa36028179d6102084efc6cb77c530db3a0c
MathNunesx <matheusjobs04@gmail.com>
2024-04-02 23:29:23 -0300

    menumobilefinalizado

--- javascript/menu.js
+++ javascript/menu.js
@@ -2,3 +2,20 @@ window.addEventListener("scroll", function(){
     let header = document.querySelector('#header')
     header.classList.toggle('rolagem', window.scrollY > 0)
 })
+
+let menuBtn = document.getElementById('btn-menu');
+let menu = document.getElementById('mobile-menu');
+let overlay = document.getElementById('menu-overlay')
+
+
+menuBtn.addEventListener('click' , function(){
+    menu.classList.add('open-menu')
+})
+
+menu.addEventListener('click' , function(){
+    menu.classList.remove('open-menu')
+})
+
+overlay.addEventListener('click' , function(){
+    menu.classList.remove('open-menu')
+})
